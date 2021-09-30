d7a18e5c94e42590becca627d1b03c154e2fa155
my4fdal <my4fdal@gmail.com>
2021-09-30 11:18:06 +0700

    1. add middleware 2. add permission is public 3. add pagination 4. add search 5. add type paginate and show data 6. other ...

--- src/Core/GenerateQueryGraphql.php
+++ src/Core/GenerateQueryGraphql.php
@@ -2,10 +2,12 @@
 
 namespace Uasoft\Badaso\Module\Graphql\Core;
 
+use Doctrine\DBAL\Types\ObjectType;
 use GraphQL\Type\Definition\Type;
 use Illuminate\Support\Facades\DB;
 use Uasoft\Badaso\Helpers\CaseConvert;
 use Uasoft\Badaso\Module\Graphql\Core\Type\PaginateType;
+use Uasoft\Badaso\Module\Graphql\Core\Type\ResultAllDataType;
 use Uasoft\Badaso\Module\Graphql\Traits\PermissionForCRUDTrait;
 
 class GenerateQueryGraphql extends \Uasoft\Badaso\Controllers\Controller
@@ -63,28 +65,104 @@ class GenerateQueryGraphql extends \Uasoft\Badaso\Controllers\Controller
         ];
     }
 
+    public function generateSearchQuery(){
+        // @search text
+        $this->fields_query[CaseConvert::camel($this->table_name . '_search')] = [
+            'type' => new PaginateType($this->browse_type, 'search_'.$this->table_name),
+            'args' => [
+                'q' => [
+                    'type' => Type::string(),
+                ],
+                'bys' => [
+                    'type' => Type::listOf(Type::string()),
+                    'defaultValue' => ['*'],
+                    'description' => "
+                        search by * for all field in table, your can add only one field\n
+                        example : ['name_file1', 'name_field2', ..., 'name_fieldN']
+                    "
+                ],
+                'page' => [
+                    'type' => Type::int(),
+                    'defaultValue' => 1,
+                ],
+                'maxDataPerPage' => [
+                    'type' => Type::int(),
+                    'defaultValue' => 15,
+                ]
+            ],
+            'resolve' => function ($rootValue, $args) {
+                $this->permissionCrud('read');
+
+                ['q' => $q, 'bys' => $bys, 'page' => $page, 'maxDataPerPage' => $max_data_per_page] = $args ;
+
+                $query = DB::table($this->table_name) ;
+
+                foreach ($bys as $key => $by) {
+                    if ($by == '*') {
+                        foreach ($this->browse_type->getFields() as $key => $field) {
+                            $field_name = $field->name;
+                            $query = $query->orWhere($field_name, "like", "%{$q}%");
+                        }
+                    } else {
+                        $query = $query->orWhere($by, "like", "%{$q}%");
+                    }
+                }
+
+                $query = $query->offset(ceil(abs($page - 1) * $max_data_per_page))->limit($max_data_per_page) ;
+
+                $data = $query->get();
+                $max_data = $query->count();
+                $max_page = ceil($max_data/$max_data_per_page) ;
+
+                return PaginateType::result($data, $max_data, $max_page) ;
+            },
+        ];
+    }
+
     public function generateAllQuery()
     {
         // @all
         if ($this->data_type->server_side) {
             $this->fields_query[$this->table_name] = function () {
                 return [
-                    'type' => new PaginateType($this->browse_type),
+                    'type' => new PaginateType($this->browse_type, $this->table_name),
+                    'args' => [
+                        'page' => [
+                            'type' => Type::int(),
+                            'defaultValue' => 1,
+                        ],
+                        'maxDataPerPage' => [
+                            'type' => Type::int(),
+                            'defaultValue' => 15,
+                        ]
+                    ],
                     'resolve' => function ($rootValue, $args) {
                         $this->permissionCrud('browse');
 
-                        return DB::table($this->table_name)->simplePaginate(15)->toArray();
+                        ['page' => $page, 'maxDataPerPage' => $max_data_per_page] = $args ;
+
+                        $query = DB::table($this->table_name)->offset(ceil(abs($page - 1) * $max_data_per_page))->limit($max_data_per_page) ;
+
+                        $max_data = $query->count();
+                        $data = $query->get();
+                        $max_page = ceil($max_data/$max_data_per_page) ;
+
+                        return PaginateType::result($data, $max_data, $max_page);
                     },
                 ];
             };
         } else {
             $this->fields_query[$this->table_name] = function () {
                 return [
-                    'type' => Type::listOf($this->browse_type),
+                    'type' => new ResultAllDataType($this->browse_type, $this->table_name),
                     'resolve' => function ($rootValue, $args) {
                         $this->permissionCrud('browse');
 
-                        return DB::table($this->table_name)->get();
+                        $query = DB::table($this->table_name) ;
+                        $data = $query->get();
+
+                        $max_data = $data->count() ;
+                        return ResultAllDataType::result($data, $max_data);
                     },
                 ];
             };
@@ -97,6 +175,7 @@ class GenerateQueryGraphql extends \Uasoft\Badaso\Controllers\Controller
     {
         $this->generateAllQuery();
         $this->generateFindQuery();
+        $this->generateSearchQuery();
 
         // add customize
         $this->customizeFieldQuery();
--- src/Core/Type/ResultAllDataType.php
+++ src/Core/Type/ResultAllDataType.php
@@ -1,37 +1,28 @@
 <?php
-
 namespace Uasoft\Badaso\Module\Graphql\Core\Type;
 
 use GraphQL\Type\Definition\ObjectType;
 use GraphQL\Type\Definition\Type;
 use Uasoft\Badaso\Helpers\CaseConvert;
 
-class PaginateType extends ObjectType
-{
+class ResultAllDataType extends ObjectType{
     public function __construct(ObjectType $dataType, string $key)
     {
         parent::__construct([
-            'name' => CaseConvert::camel('pagination_' . $key),
+            'name' =>  CaseConvert::camel('result_all_data_' . $key),
             'fields' => [
-                'data' => [
-                    'type' => Type::listOf($dataType),
-                ],
+                'data' => Type::listOf($dataType),
                 'maxData' => [
-                    'type' => Type::int(),
-                ],
-                'maxPage' => [
                     'type' => Type::int(),
                 ]
             ]
         ]);
     }
 
-    public static function result($data, $max_data, $max_page): array
-    {
+    public static function result($data, $max_data){
         return [
             'data' => $data,
             'maxData' => $max_data,
-            'maxPage' => $max_page,
         ];
     }
 }
